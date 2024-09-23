ae9cba35d5a0c5bb5b1cc8d10bdab6083922123f
Danielle Gibson <peyton.gibson@stu.barren.kyschools.us>
2024-09-23 08:02:47 -0500

    Initial Commit

---
+++ Challenge 2/Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2App.swift
+//  Challenge 2
+//
+//  Created by Student on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
